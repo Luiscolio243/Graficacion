4e9f9a1589c80abc68311debafcf431ab72c238c
Pau1824 <chaviraespinozapaulina@gmail.com>
2026-02-05 01:19:54 -0700

    cambio de version del node 20
    no podia descargar el tailwind

--- requisitos-app/src/auth/pages/Login.jsx
+++ requisitos-app/src/auth/pages/Login.jsx
@@ -2,7 +2,9 @@ export default function Login() {
     return (
         <div>
             <h1>Iniciar sesion</h1>
-            <input placeholder="Correo" />
+            <input
+                
+                placeholder="Correo" />
             <input type="password" placeholder="Contrasena" />
             <button>Entrar</button>
         </div>
