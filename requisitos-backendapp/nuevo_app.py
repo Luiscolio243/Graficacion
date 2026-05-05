dde788d89304bab20274bf56df5acc1a643e3502
Luiscolio243 <luiscolio243@gmail.com>
2026-05-04 21:19:39 -0700

    Conexion de la bd diagrama paquetes
    Aqui lo que hice es conectar la bd con el diagrama de paquetes

--- requisitos-backendapp/nuevo_app.py
+++ requisitos-backendapp/nuevo_app.py
@@ -18,6 +18,7 @@ from Routers.responderEncuesta import responder_encuesta_bp
 from Routers.historiasUsuario import historias_bp
 from Routers.ClasesUML import clases_uml_bp
 from Routers.Diagramas import diagramas_bp
+from Routers.PaquetesUML import paquetes_uml_bp
 
 Config.validate()
 
@@ -40,6 +41,7 @@ app.register_blueprint(focus_groups_bp)
 app.register_blueprint(historias_bp)
 app.register_blueprint(clases_uml_bp)
 app.register_blueprint(diagramas_bp)
+app.register_blueprint(paquetes_uml_bp)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=Config.FLASK_PORT, debug=Config.FLASK_ENV == 'development')
